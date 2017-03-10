09ce0afd0b17e65d554600b22402c1080897d1c9
Ray <izhangshuanglei@gmail.com>
2017-03-10 17:23:25 +0800

    First

---
+++ FirstPython.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+print 'hello, world'
+'''可以以,分开,输入显示空格'''
+"""这里是注释"""
+print 'hello','world'
+'''加法'''
+print 100+100
+print '100+200=',100+200
+print '请输入您的名字:'
+name=raw_input();
+print "您的名字是:",name
+"""True False"""
+print "请输入您的年龄:"
+age=raw_input();
+if age<18:
+    print name,"您还是个小孩儿,赶紧玩吧"
+elif age>18 or age<30:
+    print name,"您已经成年了,别玩了"
+else:
+    print name,"您都30了还瞎玩呢"
+"""IF ELSE"""
+
+
