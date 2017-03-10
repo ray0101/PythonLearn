bb9b3dc6c6465435dbe519a476be2c4ec8d63389
Ray <izhangshuanglei@gmail.com>
2017-03-10 18:11:15 +0800

    First

--- FirstPython.py
+++ FirstPython.py
@@ -3,22 +3,22 @@
 print 'hello, world'
 '''可以以,分开,输入显示空格'''
 """这里是注释"""
-print 'hello','world'
+print 'hello', 'world'
 '''加法'''
-print 100+100
-print '100+200=',100+200
+print 100 + 100
+print '100+200=', 100 + 200
 print '请输入您的名字:'
-name=raw_input();
-print "您的名字是:",name
+name = raw_input();
+print "您的名字是:", name
 """True False"""
-print "请输入您的年龄:"
-age=raw_input();
-if age<18:
-    print name,"您还是个小孩儿,赶紧玩吧"
-elif (age>18 and age<30):
-    print name,"您还不到30,别光玩了"
+age = int(raw_input());
+if int(age) > 30:
+    print name, "您都30多了还瞎玩呢"
+elif int(age) > 18:
+    print name, "您还不到30,别光玩了"
 else:
-    print name,"您都30多了还瞎玩呢"
+    print name, "您还是个小孩儿,赶紧玩吧"
 """IF ELSE"""
-
-
+names=['曹操','大乔','小乔']
+for name in names:
+    print name;
