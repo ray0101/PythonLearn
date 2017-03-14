33db436a9436cc0f5b09cb89fe9f152a24984bee
Ray <izhangshuanglei@gmail.com>
2017-03-14 23:02:37 +0800

    localhost

--- MyWeChat.py
+++ MyWeChat.py
@@ -1,10 +1,23 @@
 #coding=utf8
-import itchat, time
+import itchat, time,hashlib,requests
 from itchat.content import *
 
 @itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING])
 def text_reply(msg):
-    itchat.send('%s: %s' % (msg['Type'], msg['Text']), msg['FromUserName'])
+    apiUrl="http://www.tuling123.com/openapi/api"
+
+    data={
+        'key':"cdc9255cb280425789f158a38506b1a3",
+        'info':msg['Text'],
+        'userid':get_md5_value(msg['FromUserName']),
+    }
+    returnmsg='';
+    try:
+        r=requests.post(apiUrl,data=data).json()
+        returnmsg=r.get('text')
+    except:
+        returnmsg=""
+    itchat.send('%s' % returnmsg, msg['FromUserName'])
 
 @itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO])
 def download_files(msg):
@@ -20,6 +33,10 @@ def add_friend(msg):
 def text_reply(msg):
     if msg['isAt']:
         itchat.send(u'@%s\u2005I received: %s' % (msg['ActualNickName'], msg['Content']), msg['FromUserName'])
-
+def get_md5_value(src):
+    myMd5=hashlib.md5()
+    myMd5.update(src)
+    myMd5_digest=myMd5.hexdigest()
+    return myMd5_digest
 itchat.auto_login(True)
 itchat.run()
