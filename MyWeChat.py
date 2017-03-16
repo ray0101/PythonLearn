0979a0039e799466b0fde20956db69c0d4d6a2c8
Ray <izhangshuanglei@gmail.com>
2017-03-16 19:35:27 +0800

    First

--- MyWeChat.py
+++ MyWeChat.py
@@ -1,24 +1,31 @@
 #coding=utf8
-import itchat, time,hashlib,requests
+import itchat, time,hashlib,requests,sys
 from itchat.content import *
-
+default_encoding = 'utf-8'
+if sys.getdefaultencoding() != default_encoding:
+    reload(sys)
+    sys.setdefaultencoding(default_encoding)
 @itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING])
 def text_reply(msg):
     apiUrl="http://www.tuling123.com/openapi/api"
-
-    data={
-        'key':"cdc9255cb280425789f158a38506b1a3",
-        'info':msg['Text'],
-        'userid':get_md5_value(msg['FromUserName']),
-    }
-    returnmsg='';
-    try:
-        r=requests.post(apiUrl,data=data).json()
-        returnmsg=r.get('text')
-    except:
-        returnmsg=""
-    time.sleep(3)
-    itchat.send('%s' % returnmsg, msg['FromUserName'])
+    print "消息:","[",msg['FromUserName'],"]:收到",msg["Text"]
+    if "通过"in msg['Text']:
+        itchat.send('%s' % "我知道天气/星座/会讲笑话/会讲故事/绕口令哦 更多功能你可以自己发掘", msg['FromUserName'])
+    else:
+        data={
+            'key':"cdc9255cb280425789f158a38506b1a3",
+            'info':msg['Text'],
+            'userid':get_md5_value(msg['FromUserName']),
+        }
+        returnmsg='';
+        try:
+            r=requests.post(apiUrl,data=data).json()
+            returnmsg=r.get('text')
+        except:
+            returnmsg=""
+        time.sleep(3)
+        print "我回复了:", returnmsg
+        itchat.send('%s' % returnmsg, msg['FromUserName'])
 
 @itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO])
 def download_files(msg):
